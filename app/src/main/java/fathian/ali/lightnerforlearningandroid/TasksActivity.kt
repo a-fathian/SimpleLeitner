8bbe6ca01373b009a2ca8baa616e0a98f261976d
Ali Fathian <fathian.ali@gmail.com>
2020-12-29 17:48:18 +0330

    changed the name of the main activity

---
+++ app/src/main/java/fathian/ali/lightnerforlearningandroid/TasksActivity.kt
@@ -0,0 +1,11 @@
+package fathian.ali.lightnerforlearningandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TasksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
