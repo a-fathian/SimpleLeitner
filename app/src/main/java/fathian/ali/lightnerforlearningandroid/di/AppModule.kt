338b3f469d44ebc5f0472e4ab865310c7bd9075a
Ali <arash_snp@yahoo.com>
2020-12-31 16:03:55 +0330

    DatabaseBuilder injected

--- app/src/main/java/fathian/ali/lightnerforlearningandroid/di/AppModule.kt
+++ app/src/main/java/fathian/ali/lightnerforlearningandroid/di/AppModule.kt
@@ -2,6 +2,9 @@ package fathian.ali.lightnerforlearningandroid.di
 
 import dagger.Module
 import dagger.Provides
+import fathian.ali.lightnerforlearningandroid.App
+import fathian.ali.lightnerforlearningandroid.data.local.AppDatabase
+import fathian.ali.lightnerforlearningandroid.data.local.DatabaseBuilder
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 import kotlin.coroutines.CoroutineContext
@@ -14,5 +17,9 @@ class AppModule {
     fun provideCoroutineContext(): CoroutineContext {
         return Dispatchers.IO
     }
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(): AppDatabase = DatabaseBuilder.getInstance(App.context)
 
 }
