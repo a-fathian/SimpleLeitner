4f45fdf73459bc3f3bcbc66024c8ab3634dfda2b
Ali <arash_snp@yahoo.com>
2020-12-31 16:46:09 +0330

    DatabaseBuilder injected

--- app/src/main/java/fathian/ali/lightnerforlearningandroid/data/local/entity/Task.kt
+++ app/src/main/java/fathian/ali/lightnerforlearningandroid/data/local/entity/Task.kt
@@ -1,10 +1,12 @@
 package fathian.ali.lightnerforlearningandroid.data.local.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
 @Entity
 data class Task(
+    @PrimaryKey
     val id: String = UUID.randomUUID().toString(),
     val name: String,
     val description: String,
